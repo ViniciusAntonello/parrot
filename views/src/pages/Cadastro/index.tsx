c179012039e8cbf70d83600b787450e240c0a934
sabrynnall <limasabrynna03@gmail.com>
2022-06-14 12:24:11 -0300

    melhorando inputs da pagina de cadastro

--- views/src/pages/Cadastro/index.tsx
+++ views/src/pages/Cadastro/index.tsx
@@ -1,56 +1,37 @@
-import Background from '../../assets/background.png';
 import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
+import Container from 'react-bootstrap/Container';
+import logo from '../../assets/logo.png';
 import './styles.css';
 
 const Cadastro: React.FC = () => {
   return (
-    <div className='App'>
-      <main>
-      <InputGroup className="mb-3">
-        <Form.Control
-          placeholder="nome"
-          aria-label="nome"
-          aria-describedby="basic-addon1"
-        />
-      </InputGroup>
-      <InputGroup className="mb-3">
-        <Form.Control
-          placeholder="email"
-          aria-label="email"
-          aria-describedby="basic-addon1"
-        />
-      </InputGroup>
-      <InputGroup className="mb-3">
-        <Form.Control
-          placeholder="senha"
-          aria-label="senha"
-          aria-describedby="basic-addon1"
-        />
-      </InputGroup>
-      <InputGroup className="mb-3">
-        <Form.Control
-          placeholder="confirmar senha"
-          aria-label="confirmar senha"
-          aria-describedby="basic-addon1"
-        />
-      </InputGroup>
-      <InputGroup className="mb-3">
-        <Form.Control
-          placeholder="unidade/apartamento"
-          aria-label="unidade/apartamento"
-          aria-describedby="basic-addon1"
-        />
-      </InputGroup>
-      <InputGroup className="mb-3">
-        <Form.Control
-          placeholder="link da foto"
-          aria-label="link da foto"
-          aria-describedby="basic-addon1"
-        />
-      </InputGroup>
-      </main>
-    </div>
+      <Container id='main-container' className='d-flex justify-content-center align-items-center'>
+          <Form className='text-center'>
+            <img 
+            className='parrot-logo' 
+            src={logo} 
+            alt="logo parrot" />
+            <h1>CADASTRO</h1>
+            <Form.Group>
+              <Form.Control type='text' size='sm' placeholder='nome' className='input-cadastro'/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Control type='email' size='sm' placeholder='email'className='input-cadastro'/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Control type='text' size='sm' placeholder='senha' className='input-cadastro'/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Control type='text' size='sm' placeholder='confirmar senha' className='input-cadastro'/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Control type='text' size='sm' placeholder='unidade/apartamento' className='input-cadastro'/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Control type='text' size='sm' placeholder='link da foto' className='input-cadastro'/>
+            </Form.Group>
+          </Form>
+      </Container>
   );
 }
 
